f3766ddbc519a61032edefb070bffc745db2a436
Bo-Yuan Huang <byhuang1992@gmail.com>
2019-05-11 15:43:11 -0400

    RX interface signal info

--- include/lmac/core2/configs_spec.h
+++ include/lmac/core2/configs_spec.h
@@ -17,6 +17,8 @@
 
 namespace ilang {
 
+// ------------------------- TX FIFO Interface ------------------------------ //
+
 // Write enable.  1 = data is valid on the TX_DATA bus. 0 = no data is on the
 // data bus
 #define TX_WE "TX_WE"
@@ -61,6 +63,66 @@ namespace ilang {
 // [9:0] for FIFO size 1Kx64)
 #define TXFIFO_WUSED_QWD "TXFIFO_WUSED_QWD"
 #define TXFIFO_WUSED_QWD_BWID 13
+
+// ------------------------- RX FIFO Interface ------------------------------ //
+// 1. Receiving Bus - package byte count/size interface (IPCS)
+// 2. Receiving Bus - package data interface. (after reading the byte count the
+//    user can read out the packet's data - 1 entire packet at a time)
+
+// Byte count FIFO’s empty signal.  0 = a byte count is available for reading. 1
+// = no byte count is available
+#define RX_IPCS_EMPTY "RX_IPCS_EMPTY"
+#define RX_IPCS_EMPTY_BWID 1
+#define RX_IPCS_EMPTY_V_READY 0
+#define RX_IPCS_EMPTY_V_NONE 1
+
+// Read enable for the byte count FIFO.
+// User asserts 1 pulse to read 1 qword from the byte count FIFO for the size of
+// 1 packet.
+#define RX_IPCS_RDEN "RX_IPCS_RDEN"
+#define RX_IPCS_RDEN_BWID 1
+#define RX_IPCS_RDEN_V_READ 1
+
+// 64-bit IPCS data bus
+// The IPCS bus is only intended for reading 1 qword at a time.
+// Currently:
+// [63:48] = packet’s byte count
+// [47:0] = reserve
+#define RX_IPCS_DATA "RX_IPCS_DATA"
+#define RX_IPCS_DATA_BWID 64
+#define RX_IPCS_PKT_BYTE_CNT_BEG 48
+#define RX_IPCS_PKT_BYTE_CNT_END 63
+
+// Packet data FIFO’s empty signal.  0 = a packet is available for reading. 1 =
+// no packet is available
+#define RX_PKT_EMPTY "RX_PKT_EMPTY"
+#define RX_PKT_EMPTY_BWID 1
+#define RX_PKT_EMPTY_V_READY 0
+#define RX_PKT_EMPTY_V_NONE 1
+
+// Read enable for the packet data FIFO. User asserts 1 pulse to read 1 qword
+// from the packet data FIFO.
+#define RX_PKT_RDEN "RX_PKT_RDEN"
+#define RX_PKT_RDEN_BWID 1
+#define RX_PKT_RDEN_V_READ 1
+
+// Indicates a user packet read cycle is in progress.  This signal is used to
+// indicate to the LMAC that a user packet read cycle is in progress which can
+// be lengthy depending on the size of the packet.  Asserts high to start the
+// cycle and only negates when the user’s cycle is fully complete.
+#define RX_PKT_RD_CYCLE "RX_PKT_RD_CYCLE"
+#define RX_PKT_RD_CYCLE_BWID 1
+#define RX_PKT_RD_CYCLE_BUSY 1
+#define RX_PKT_RD_CYCLE_DONE 0
+
+// Packet’s data bus.
+// For each RDEN a qword is sent out to the user from the packet data FIFO.  The
+// full qword is always provided.  Based on the packet’s byte count the user can
+// determines how many bytes is available or remaining to be read from the
+// packet. Similar to FPGA FIFO, it takes 1 clock for data to be available from
+// the read enable.
+#define RX_PKT_DATA "RX_PKT_DATA"
+#define RX_PKT_DATA_BWID 64
 
 }; // namespace ilang
 
