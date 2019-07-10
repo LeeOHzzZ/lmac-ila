70d6a4614ad1e616774fd3574b47ec573d60aac0
LeeOH <LeeOH@nat-oitwireless-inside-vapornet100-10-9-107-108.princeton.edu>
2019-07-10 17:52:45 -0400

    update 1 instr

--- src/core2/tx_instr.cc
+++ src/core2/tx_instr.cc
@@ -24,6 +24,12 @@ void WrPktPayLoad(Ila& m, const std::string& name = "TX_WR_PKT_PAY_LOAD");
 void WrPktLastOne(Ila& m, const std::string& name = "TX_WR_PKT_LAST_ONE");
 
 void LmacCore2::SetupTxInstr(Ila& m) {
+
+  /********** Added by Yi Li **************/
+  // Write bytes into FIFO
+  WrPktFIFO(m);
+  /********** Added by Yi Li **************/
+
   // send the byte count of the packet
   WrPktByteCnt(m);
 
@@ -35,6 +41,29 @@ void LmacCore2::SetupTxInstr(Ila& m) {
 
   return;
 }
+
+/********** Added by Yi Li **************/
+void WrPktFIFO(Ila& m, const std::string& name) {
+  // This instruction model the writing data into FIFO of TX
+  {
+    auto instr = m.NewInstr("WR_PKT_DATA_FIFO")；
+
+    // decode
+    auto wr_enable = (m.input(TX_WE) == TX_WE_V_VALID);
+    auto fifo_non_full = (m.state(TXFIFO_FULL) != TXFIFO_FULL_V_FULL);
+    instr.SetDecode(wr_enable && fifo_non_full);
+
+    // update
+    instr.SetUpdate( storage, Store(storage, wptr(FIFO_IDX_WIDTH-1,0), w_data_in) );
+
+    instr.SetUpdate(TXFIFO_BUFF, Store(TXFIFO_BUFF, TXFIFO_BUFF_WR_PTR, m.input(TX_DATA)) );
+    instr.SetUpdate(m.state(TXFIFO_BUFF_WR_PTR), m.state(TXFIFO_BUFF_WR_PTR) + 1);
+    instr.SetUpdate(m.state(TXFIFO_WUSED_QWD), m.state(TXFIFO_WUSED_QWD) + 1);
+    instr.SetUpdate(m.state(TX_FIFO_FULL), Ite((m.state(TXFIFO_WUSED_QWD) == 1024), 1, 0));
+  }
+}
+
+
 
 void WrPktByteCnt(Ila& m, const std::string& name) {
   // handle the bytecount of the ethernet packet. The first QWord doesn't go to the output. only contain the byte count information for control and record
