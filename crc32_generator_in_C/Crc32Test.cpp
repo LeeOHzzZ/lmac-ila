fbd20f2851218c7c5e4b559072b21bfa0ae2327c
Yi Li <yi@Yis-MacBook-Pro.local>
2019-07-25 11:42:25 -0400

    crc generation update. Modified initial value. pass bytes = 7

--- crc32_generator_in_C/Crc32Test.cpp
+++ crc32_generator_in_C/Crc32Test.cpp
@@ -51,21 +51,52 @@ static double seconds()
 #endif
 }
 
+// byte 47 data:
+// 602caf4e00321200 004500085bc8b40c c807a8c0f4ffff32 a8c0f4ffff321200 000000000000c807 0000000000000000
+// real_input: 2caf4e0032120000 4500085bc8b40c60 07a8c0f4ffff3200 c0f4ffff321200c8 0000000000c807a8 0000000000000000
+//  uint64_t data_in[6] = {0x2caf4e0032120000, 0x4500085bc8b40c60, 0x07a8c0f4ffff3200, 0xc0f4ffff321200c8, 0x0000000000c807a8, 0x0000000000000000};
 
+// byte 49 data:
+// 602caf4e00321200 00450008f0ffff32 12000b0419270008 a8c06408a8c02f64 0680004000005804 18507c0a773f7211 000058a3bc0cbc08 
+// real_input:  0000000000000000 602caf4e00321232 00450008f0ffff08 12000b0419270064 a8c06408a8c02f04 0680004000005811 18507c0a773f7208 00000058a3bc0cbc?
 int main(int, char**)
 {
   printf("Please wait ...\n");
 
   uint32_t randomNumber = 0x27121978;
-  uint64_t data_in[6] = {0x602caf4e00321200, 0x004500085bc8b40c, 0x1200ffffffffffff, 0x01000608f4ffff32, 0x1200020004060008, 0xa8c0f4ffff321200};
+  // uint64_t data_in[7] = {0x0000000000000000, 0x00450008f0ffff08, 0x12000b0419270064, 0xa8c06408a8c02f04, 0x0680004000005811, 0x18507c0a773f7208};
+  uint64_t data_in[6] = {0x2caf4e0032120000, 0x4500085bc8b40c60, 0x07a8c0f4ffff3200, 0xc0f4ffff321200c8, 0x0000000000c807a8, 0x0000000000000000};
+
+  uint32_t initial_crc[8] = {0x00000000, 0x56a579b9, 0xe962b350, 0xe962b350, 0x9d0ad96d, 0x7ed9d15c, 0x6f62e365, 0x26706a0f};
+  uint32_t initial_crc_translate;
+
+  for (int i = 0; i < 8; i++) {
+    initial_crc_translate = ~(((initial_crc[i] >> 24) & 0x000000FF) | ((initial_crc[i] >> 8) & 0x0000FF00) | ((initial_crc[i] << 8) & 0x00FF0000) | ((initial_crc[i] << 24) & 0xFF000000));
+  printf("initial_crc_translation  : bytes_remain = %d, initial_CRC =%08x\n", i, initial_crc_translate);
+  }
+
+  // initial_crc_translation  : bytes_remain = 0, initial_CRC =ffffffff
+  // initial_crc_translation  : bytes_remain = 1, initial_CRC =46865aa9
+  // initial_crc_translation  : bytes_remain = 2, initial_CRC =af4c9d16
+  // initial_crc_translation  : bytes_remain = 3, initial_CRC =af4c9d16
+  // initial_crc_translation  : bytes_remain = 4, initial_CRC =9226f562
+  // initial_crc_translation  : bytes_remain = 5, initial_CRC =a32e2681
+  // initial_crc_translation  : bytes_remain = 6, initial_CRC =9a1c9d90
+  // initial_crc_translation  : bytes_remain = 7, initial_CRC =f0958fd9
+
   // uint64_t data_in_r[8] = {0x00212300e4fac206, 0x004500085bc8b40c, 0x1200ffffffffffff, 0x01000608f4ffff32, 0x1200020004060008, 0xa8c0f4ffff321200};
   char* data = new char[NumBytes];
   const uint8_t* current = (const uint8_t*) data;
 
   uint32_t crc = 0x00000000;
   uint32_t previous_crc;
-  uint32_t original_crc = 0xFFFFFFFF; 
+
+  // my initial crc code is different from the original design.
+  uint32_t original_crc = 0xf0958fd9; 
   uint32_t crc_out;
+
+  uint32_t initial_crc_out = ~(((original_crc >> 24) & 0x000000FF) | ((original_crc >> 8) & 0x0000FF00) | ((original_crc << 8) & 0x00FF0000) | ((original_crc << 24) & 0xFF000000));
+  printf("half_byte_initial_crc_out  :  CRC =%#08x\n", initial_crc_out);
 
   for (int j = 0; j < 6; j++) {
 
@@ -75,16 +106,17 @@ int main(int, char**)
       // data[i] = char((data_in[0] >> (8*(7-i))) & 0xFF);
       data[i] = char((data_in[j] >> (8*i)) & 0xFF);
     }
-    for (int i = 0; i < NumBytes; i++) {
-      // printf("test_input : data[%d] = %#02x\n", i, *current);
-      current++;
-    }
+    // for (int i = 0; i < NumBytes; i++) {
+    //   // printf("test_input : data[%d] = %#02x\n", i, *current);
+    //   current++;
+    // }
 
 
-    previous_crc = original_crc;
     #ifdef CRC32_TEST_HALFBYTE
     // half-byte
+    previous_crc = original_crc;
     original_crc = crc32_halfbyte(data, NumBytes, previous_crc);
+
     // crc generator use the orignal output of the last result, but not the output crc code.
     // The output crc code need to change the endian and reverse all the bits.
     crc_out = ~(((original_crc >> 24) & 0x000000FF) | ((original_crc >> 8) & 0x0000FF00) | ((original_crc << 8) & 0x00FF0000) | ((original_crc << 24) & 0xFF000000));
